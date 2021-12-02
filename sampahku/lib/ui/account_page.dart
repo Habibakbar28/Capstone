0a347747d5d09f895fd778f471e447d1ea442ea2
Habib Akbar Wibowo <80377952+Habibakbar28@users.noreply.github.com>
2021-12-02 16:41:22 +0700

    Update account_page.dart
    mengubah text halaman akun

--- sampahku/lib/ui/account_page.dart
+++ sampahku/lib/ui/account_page.dart
@@ -6,7 +6,7 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Account Page'),
+        child: Text('Halaman akun'),
       ),
     );
   }
